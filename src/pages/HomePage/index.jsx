f9d4261bb56cce6b614da9dc17a9a6ee8f82c5a6
daeun <rlaekdms901@naver.com>
2025-08-30 21:28:06 +0900

    feat: 공통 Footer 구현

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,10 @@
+import Footer from '../../components/layout/Footer';
+
+export default function HomePage() {
+  return (
+    <div className="h-full bg-background">
+      <div></div>
+      <Footer />
+    </div>
+  );
+}
