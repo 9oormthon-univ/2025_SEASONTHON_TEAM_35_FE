911aba099e359c0d835a7ba0b76071fbf2dad47b
daeun <rlaekdms901@naver.com>
2025-08-30 22:40:11 +0900

    feat: 정보 미입력 컴포넌트 구현

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,18 @@
+import NoInfo from '../../components/NoInfo/NoInfo';
 import Footer from '../../components/layout/Footer';
 
 export default function HomePage() {
   return (
-    <div className="h-full bg-background">
+    <div className="h-full bg-background flex flex-col items-center">
       {/* layout */}
       {/* content */}
+      <NoInfo
+        title="아직 자산을 입력하지 않았습니다."
+        description="보유하신 자산을 입력해주세요!
+"
+        btnText="자산 입력하기"
+        link="inform"
+      />
       {/* footer */}
       <Footer />
     </div>
