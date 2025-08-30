10e5314876b69e6d8f82f1969927f15e16c4aa1f
daeun <rlaekdms901@naver.com>
2025-08-30 21:30:49 +0900

    refactor: 불필요한 코드 제거

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,7 +3,9 @@ import Footer from '../../components/layout/Footer';
 export default function HomePage() {
   return (
     <div className="h-full bg-background">
-      <div></div>
+      {/* layout */}
+      {/* content */}
+      {/* footer */}
       <Footer />
     </div>
   );
