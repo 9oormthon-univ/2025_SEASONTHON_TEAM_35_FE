a8bffc551d1a3d87a10e47a725abf851f9ca1f5e
daeun <rlaekdms901@naver.com>
2025-09-03 15:56:56 +0900

    refactor: layout 수정

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,16 +2,13 @@ import Header from './Header';
 import Footer from './Footer';
 
 export default function Layout({ children, title }) {
-    return (
-        <div className="bg-background h-screen overflow-y-auto relative">
-            <Header title={title} />
+  return (
+    <div className="bg-background h-[852px] overflow-y-auto relative">
+      <Header title={title} />
 
-            {/* 스크롤 때문에 Footer 높이만큼 padding-bottom */}
-            <main className="px-7 pb-[100px]">
-                {children}
-            </main>
+      <main className="px-7 h-[654px] overflow-y-auto">{children}</main>
 
-            <Footer />
-        </div>
-    );
+      <Footer />
+    </div>
+  );
 }
