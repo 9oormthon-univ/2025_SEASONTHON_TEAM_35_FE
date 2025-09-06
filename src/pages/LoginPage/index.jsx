03fe248f62d75dd5fbcd90fea18e7a0b774b4579
daeun <rlaekdms901@naver.com>
2025-09-06 17:18:59 +0900

    feat: 로그인 api 연결

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,16 +1,19 @@
-import { Link } from 'react-router-dom';
-
 import kakaoIcon from '../../assets/Login/kakaoIcon.png';
 
 export default function LoginPage() {
+  const handleKaKaoLogin = () => {
+    window.location.href =
+      'https://growplanserver.shop/oauth2/authorization/kakao';
+  };
   return (
     <div className="h-full flex flex-col justify-end">
       <div className="flex flex-col font-bold text-[30px] mb-[317px] ml-[24px]">
         <h1 className="text-gray-100">AI 맞춤형 자산관리</h1>
         <h2 className="text-primary-2">그로우플랜</h2>
       </div>
-      <Link
-        to="/on-boarding"
+      <button
+        // to="/on-boarding"
+        onClick={handleKaKaoLogin}
         className="w-[350px] h-[56px] rounded-[12px] bg-[#FFEB3B] flex text-[16px] text-[#111111] font-bold justify-center items-center mb-[179px] self-center"
       >
         <img
@@ -19,7 +22,7 @@ export default function LoginPage() {
           className="w-[32px] h-[27px] mr-[6px]"
         />
         카카오톡으로 시작하기
-      </Link>
+      </button>
     </div>
   );
 }
---
+++ src/pages/auth/KaKaoCallback.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+export default function KakaoCallback() {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = searchParams.get('token');
+    if (token) {
+      // 토큰 저장
+      localStorage.setItem('accessToken', token);
+      // 로그인 성공 후 이동할 페이지 (예: 온보딩)
+      navigate('/on-boarding');
+    } else {
+      console.error('카카오 로그인 실패: 토큰 없음');
+      navigate('/login');
+    }
+  }, [searchParams, navigate]);
+
+  return <div>로그인 처리 중...</div>;
+}
+
+// redirect 주소 : http://localhost:5173/oauth/callback/kakao
