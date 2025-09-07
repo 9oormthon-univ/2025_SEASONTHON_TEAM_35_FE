c9b23be86b8587bdbfb3d246cb7fd6504a2f21d0
minseo <kmssally05@gmail.com>
2025-09-07 09:11:02 +0900

    merge

--- src/components/AIAssetPlanPage/InvestmentAnalysis/AIInvestmentOpinion.jsx
+++ src/components/AIAssetPlanPage/InvestmentAnalysis/AIInvestmentOpinion.jsx
@@ -9,7 +9,7 @@ export default function AIInvestmentOpinion({ data }) {
           <h1 className="text-[16px] text-gray-90 font-bold">AI 투자 의견</h1>
         </div>
         <p className="text-gray-100 text-[16px] font-medium">
-          {data?.reasonText}
+          이 포트폴리오는 안정성을 최우선으로 생각하는 보수적인 투자자에게 적합합니다. 채권 비중이 압도적으로 높아 변동성이 매우 낮지만, 그만큼 수익률 또한 제한적일 수 있습니다. 최대낙폭이 적어 시장 하락 시에도 비교적 잘 버텨낼 수 있으며, 샤프지수가 높아 위험 대비 수익률은 괜찮은 편입니다. 빠르게 자산을 늘리기보다는 꾸준히 자산을 지키고 싶은 분들에게 적합한 포트폴리오입니다.
         </p>
       </div>
     </div>
